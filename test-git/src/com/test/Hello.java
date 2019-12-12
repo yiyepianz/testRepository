75d5d02b6dd7c9b4f69dafc770a9f27110fc36ac
杨尚贤 <821434464@qq.com>
2019-12-12 10:47:04 +0800

    提交到本地

---
+++ test-git/src/com/test/Hello.java
@@ -0,0 +1,5 @@
+package com.test;
+
+public interface Hello {
+	void hello();
+}
--- test-git/src/com/test/Test.java
+++ test-git/src/com/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("第一次使用git");
-		System.out.println("第二次使用git");
+		System.out.println("第二次使用git---");
 	}
 
 }
